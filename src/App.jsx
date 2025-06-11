cbaada975380dc638090ee89de22d2456f87d67a
sejaldewangan <shubham.dewangan.3440@gmail.com>
2025-06-11 09:56:38 +0530

    handling checkboxes

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Input from './components/Input'
 const App = () => {
   return (
     <div className='bg-slate-300 w-[100%] h-[100%]'>
-      <h1 className='text-8xl'>hello</h1>
+      {/* <h1 className='text-8xl'>hello</h1>
       <h1 className='text-8xl'>i am sejal</h1>
-      <h1 className='text-8xl'>dewangan</h1>
-       <Friut />
-       <Toggle/>
+      <h1 className='text-8xl'>dewangan</h1> */}
+       {/* <Friut />
+       <Toggle/> */}
 <Input />
     </div>
   )
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Input = () => {
-    const [input,setInput]=useState("")
+  const [input, setInput] = useState([]);
+  const handleInput = (event) => {
+    console.log(event.target.value, event.target.checked);
+    if (event.target.checked === true) {
+      setInput([...input, event.target.value]);
+    } else {
+      setInput(input.filter((item) => item != event.target.value));
+    }
+  };
   return (
     <div>
-      <input value={input} onChange={(event)=>{setInput(event.target.value)}} type="text" placeholder='enter your name' />
+      {/* <input
+        value={input}
+        onChange={(event) => {
+          setInput(event.target.value);
+        }}
+        type="text"
+        placeholder="enter your name"
+      />
       <hr />
       {console.log(input)}
+      <h1>{input}</h1> */}
+      <input onChange={handleInput} type="checkbox" id="A" value={"A"} />{" "}
+      <label htmlFor="A">A</label>
+      <hr />
+      <input onChange={handleInput} type="checkbox" id="B" value={"B"} />{" "}
+      <label htmlFor="B">B</label>
+      <hr />
+      <input onChange={handleInput} type="checkbox" id="C" value={"C"} />{" "}
+      <label htmlFor="C">C</label>
+      <hr />
+      {/* <input onChange={handleInput} type="checkbox" id="a" value={a} />{" "}
+      <label htmlFor="a">A</label>
+      <hr />
+      <input onChange={handleInput} type="checkbox" id="a" value={a} />{" "}
+      <label htmlFor="a">A</label>
+      <hr />
+      <input onChange={handleInput} type="checkbox" id="a" value={a} />{" "}
+      <label htmlFor="a">A</label>
+      <hr />
+      <input onChange={handleInput} type="checkbox" id="a" value={a} />{" "}
+      <label htmlFor="a">A</label>
+      <hr />
+      <input onChange={handleInput} type="checkbox" id="a" value={a} />{" "}
+      <label htmlFor="a">A</label>
+      <hr /> */}
+      <h1>{input}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
