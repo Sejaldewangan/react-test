a07cd639615d5b8bc0085f6b377cc2ac9d423faf
sejaldewangan <shubham.dewangan.3440@gmail.com>
2025-06-25 18:11:44 +0530

    redux

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,6 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
 import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../features/counter/counterSlice";
 
 export const Clock = () => {
+  // const {value} =  useSelector((slices)=>slices.cart)
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   const [clock, setClock] = useState(0);
   useEffect(() => {
     setInterval(() => {
@@ -10,6 +16,25 @@ export const Clock = () => {
   return (
     <div>
       <h1>{clock}</h1>
+      <div>
+        {/* <h1>{value}</h1> */}
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <br />
+        <span>{count}</span>
+        <br />
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <br />
+      </div>
     </div>
   );
 };
